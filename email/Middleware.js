f4e1fde3f80ff1e3e76facd34c9b624aef3a989f
Debashismitra01 <mdebashis268@gmail.com>
2026-07-06 11:08:07 +0530

    Added /health exception for email service

--- email/Middleware.js
+++ email/Middleware.js
@@ -1,8 +1,15 @@
 const middleware = (req,res,next) =>{
+    // Health check endpoint
+    if (req.path === "/health") {
+        return res.status(200).send("OK");
+    }
+
     const api_key=req.header("email-api-key");
-    console.log(api_key);
+
     const secret = process.env.API_KEY;
+
     if(api_key!=secret) res.status(403).send("Invalid API KEY");
     else next();
 }
+
 export default middleware;
